4aec5f88107483d24b83307b8faa8e38a41f1358
randikasptra <saputrarandika857@gmail.com>
2025-05-28 11:20:43 +0700

    add/folder-admin-user

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,4 +5,9 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/', 'Home::index');
+$routes->get('/', 'Auth::login');
+$routes->post('/login', 'Auth::doLogin');
+$routes->get('/logout', 'Auth::logout');
+
+$routes->get('/dashboard/admin', 'Dashboard::admin');
+$routes->get('/dashboard/user', 'Dashboard::user');
